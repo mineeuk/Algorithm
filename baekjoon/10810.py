f849bf3d4bf037aebc32e1b161ee72f814c565f4
Yumin Kim <yumin@Yuminui-MacBookPro.local>
2024-09-08 22:43:31 +0900

    24-09-08

--- baekjoon/10810.py
+++ baekjoon/10810.py
@@ -4,12 +4,12 @@ n, m = map(int, input().split())
 
 baskets = [0] * n
 
-for i in range(m):
+for _ in range(m):
     i, j, k = map(int, input().split())
     for l in range(i-1, j):
-        baskets[l] = k # i번 바구니부터 j번 바구니까지에 k번 번호, 바구니에 공을 넣는 작업을 수행
+        baskets[l] = k # i번 바구니부터 j번 바구니까지에 k번 번호
         
-for i in range(n):
-    print(baskets[i], end=' ')
+for x in range(n):
+    print(baskets[x], end=' ')
     # 1번 바구니부터 N번 바구니에 들어있는 공의 번호를 공백으로 구분해 출력
     
